9033851b2e4ab71ae730fdb8f7bb701d7ef49a8f
Timo von Holtz <tvh@tvholtz.de>
2018-02-05 22:07:43 +0100

    Add a very primitive camera

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,22 @@ extern crate clap;
 extern crate palette;
 extern crate test;
 
-use image::*;
+use euclid::*;
+use palette::*;
 use std::fs::File;
 use clap::{Arg, App};
+use num_traits::float::*;
 
 mod ray;
 mod color;
+
+use ray::Ray;
+
+fn color(r: ray::Ray<f32>) -> Rgb<f32> {
+    let unit_direction = r.direction.normalize();
+    let t = (unit_direction.y + 1.0)*0.5;
+    Rgb::new(1.0, 1.0, 1.0)*(1.0-t) + Rgb::new(0.5, 0.7, 1.0)*t
+}
 
 fn main() {
     let matches =
@@ -26,20 +36,28 @@ fn main() {
     let output = matches.value_of("output").unwrap();
     let height = 200;
     let width = 400;
-    let mut buffer = ImageBuffer::new(width, height);
+    let mut buffer = image::ImageBuffer::new(width, height);
 
+    let lower_left_corner = Point3D::new(-2.0, -1.0, -1.0);
+    let horizontal = Vector3D::new(4.0, 0.0, 0.0);
+    let vertical = Vector3D::new(0.0, 2.0, 0.0);
+    let origin = Point3D::new(0.0, 0.0, 0.0);
     for i in 0..width {
         for j in 0..height {
+            let u = (i as f32) / (width as f32);
+            let v = (j as f32) / (height as f32);
+            let r = Ray::new(origin, origin - (lower_left_corner + horizontal*u + vertical*v));
+            let col = color(r);
             let pixel =
-                [((i as f32)/(width as f32)*255.99) as u8
-                ,((j as f32)/(height as f32)*255.99) as u8
-                ,(0.2*255.9) as u8
+                [(col.red*255.99) as u8
+                ,(col.green*255.99) as u8
+                ,(col.blue*255.9) as u8
                 ];
-            buffer[(i,j)] = Rgb(pixel);
+            buffer[(i,j)] = image::Rgb(pixel);
         }
     }
 
     let ref mut fout = File::create(output).unwrap();
 
-    ImageRgb8(buffer).save(fout, PNG).unwrap();
+    image::ImageRgb8(buffer).save(fout, image::PNG).unwrap();
 }
--- src/ray.rs
+++ src/ray.rs
@@ -11,6 +11,10 @@ impl<T> Ray<T>
 where
     T: Float
 {
+    pub fn new(origin: Point3D<T>, direction: Vector3D<T>) -> Ray<T> {
+        Ray{origin, direction}
+    }
+
     pub fn point_at_parameter(self, t: T) -> Point3D<T> {
         self.origin + self.direction*t
     }

5b7ca980bb81e7d51ca404bebc52f3b64a02a309
Timo von Holtz <tvh@tvholtz.de>
2018-02-12 21:04:17 +0100

    Massively improve speed of AABB hit check

--- src/hitable/mod.rs
+++ src/hitable/mod.rs
@@ -27,16 +27,16 @@ impl<T: CoordinateBase> BoundingBox<T> {
     pub fn intersects(&self, r: Ray<T>, t_min: T, t_max: T) -> bool {
         match self {
             &BoundingBox { low, high } => {
-                let t_x0 = (low.x - r.origin.x) / r.direction.x;
-                let t_x1 = (high.x - r.origin.x) / r.direction.x;
+                let t_x0 = (low.x - r.origin.x) * r.inv_direction.x;
+                let t_x1 = (high.x - r.origin.x) * r.inv_direction.x;
                 let t_min = T::max(t_min, T::min(t_x0, t_x1));
                 let t_max = T::min(t_max, T::max(t_x0, t_x1));
-                let t_y0 = (low.y - r.origin.y) / r.direction.y;
-                let t_y1 = (high.y - r.origin.y) / r.direction.y;
+                let t_y0 = (low.y - r.origin.y) * r.inv_direction.y;
+                let t_y1 = (high.y - r.origin.y) * r.inv_direction.y;
                 let t_min = T::max(t_min, T::min(t_y0, t_y1));
                 let t_max = T::min(t_max, T::max(t_y0, t_y1));
-                let t_z0 = (low.z - r.origin.z) / r.direction.z;
-                let t_z1 = (high.z - r.origin.z) / r.direction.z;
+                let t_z0 = (low.z - r.origin.z) * r.inv_direction.z;
+                let t_z1 = (high.z - r.origin.z) * r.inv_direction.z;
                 let t_min = T::max(t_min, T::min(t_z0, t_z1));
                 let t_max = T::min(t_max, T::max(t_z0, t_z1));
                 t_max >= t_min
@@ -85,4 +85,39 @@ pub trait Hitable<T: CoordinateBase> {
     }
     fn bbox(&self) -> BoundingBox<T>;
     fn hit(&self, r: Ray<T>, t_min: T, t_max: T) -> Option<HitRecord<T>>;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test::*;
+    use num_traits::Float;
+
+    #[bench]
+    fn bench_intersect_aabb_hit_f32(bench: &mut Bencher) {
+        let ray = black_box(Ray::new(point3(-3.0, 0.0, 0.0), vec3(1.0, 0.0, 0.0), 500.0));
+        let aabb = black_box(BoundingBox {low: point3(-1.0, -1.0, -1.0), high: point3(1.0, 1.0, 1.0)});
+        bench.iter(|| aabb.intersects(ray, f32::epsilon(), f32::max_value()));
+    }
+
+    #[bench]
+    fn bench_intersect_aabb_miss_f32(bench: &mut Bencher) {
+        let ray = black_box(Ray::new(point3(-3.0, 0.0, 0.0), vec3(1.0, 0.0, 0.0), 500.0));
+        let aabb = black_box(BoundingBox::empty());
+        bench.iter(|| aabb.intersects(ray, f32::epsilon(), f32::max_value()));
+    }
+
+    #[bench]
+    fn bench_intersect_aabb_hit_f64(bench: &mut Bencher) {
+        let ray = black_box(Ray::new(point3(-3.0, 0.0, 0.0), vec3(1.0, 0.0, 0.0), 500.0));
+        let aabb = black_box(BoundingBox {low: point3(-1.0, -1.0, -1.0), high: point3(1.0, 1.0, 1.0)});
+        bench.iter(|| aabb.intersects(ray, f64::epsilon(), f64::max_value()));
+    }
+
+    #[bench]
+    fn bench_intersect_aabb_miss_f64(bench: &mut Bencher) {
+        let ray = black_box(Ray::new(point3(-3.0, 0.0, 0.0), vec3(1.0, 0.0, 0.0), 500.0));
+        let aabb = black_box(BoundingBox::empty());
+        bench.iter(|| aabb.intersects(ray, f64::epsilon(), f64::max_value()));
+    }
 }
--- src/ray.rs
+++ src/ray.rs
@@ -1,11 +1,14 @@
 use euclid::*;
 use num_traits::float::*;
 
+pub struct Inverted;
+
 #[derive(PartialEq, Debug, Clone, Copy)]
 pub struct Ray<T> {
     pub origin: Point3D<T>,
     pub direction: Vector3D<T>,
     pub wl: f32,
+    pub inv_direction: TypedVector3D<T, Inverted>,
 }
 
 impl<T> Ray<T>
@@ -13,10 +16,34 @@ where
     T: Float
 {
     pub fn new(origin: Point3D<T>, direction: Vector3D<T>, wl: f32) -> Ray<T> {
-        Ray{origin, direction, wl}
+        let inv_direction =
+            vec3(direction.x.recip(), direction.y.recip(), direction.z.recip());
+        Ray{origin, direction, wl, inv_direction}
     }
 
     pub fn point_at_parameter(self, t: T) -> Point3D<T> {
         self.origin + self.direction*t
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test::*;
+
+    #[bench]
+    fn bench_ray_new_f32(bench: &mut Bencher) {
+        let origin = black_box(point3(-3.0, 0.0, 0.0));
+        let direction = black_box(vec3(1.0, 0.0, 0.0));
+        let wl = black_box(500.0);
+        bench.iter(|| Ray::<f32>::new(origin, direction, wl) );
+    }
+
+    #[bench]
+    fn bench_ray_new_f64(bench: &mut Bencher) {
+        let origin = black_box(point3(-3.0, 0.0, 0.0));
+        let direction = black_box(vec3(1.0, 0.0, 0.0));
+        let wl = black_box(500.0);
+        bench.iter(|| Ray::<f64>::new(origin, direction, wl) );
     }
 }
